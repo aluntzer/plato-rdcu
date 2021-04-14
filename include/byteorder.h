1fe0c94829379312f2d637771d8cc7fd5e413087
Dominik Loidolt <dominik.loidolt@univie.ac.at>
2021-04-14 10:56:39 +0200

    add support for uint64_t and clang compiler in byteorder.h

--- include/byteorder.h
+++ include/byteorder.h
@@ -72,14 +72,22 @@
 	(((uint32_t)(x) & (uint32_t)0x00ff0000UL) >>  8) |	\
 	(((uint32_t)(x) & (uint32_t)0xff000000UL) >> 24)))
 
+#define ___constant_swab64(x) ((uint64_t)(			\
+	(((uint64_t)(x) & (uint64_t)0x00000000000000ffULL) << 56) |	\
+	(((uint64_t)(x) & (uint64_t)0x000000000000ff00ULL) << 40) |	\
+	(((uint64_t)(x) & (uint64_t)0x0000000000ff0000ULL) << 24) |	\
+	(((uint64_t)(x) & (uint64_t)0x00000000ff000000ULL) <<  8) |	\
+	(((uint64_t)(x) & (uint64_t)0x000000ff00000000ULL) >>  8) |	\
+	(((uint64_t)(x) & (uint64_t)0x0000ff0000000000ULL) >> 24) |	\
+	(((uint64_t)(x) & (uint64_t)0x00ff000000000000ULL) >> 40) |	\
+	(((uint64_t)(x) & (uint64_t)0xff00000000000000ULL) >> 56)))
 
-#ifdef USE_BUILTIN_BSWAP
-#if GCC_VERSION >= 40400
+#if GCC_VERSION >= 40400 || defined(__clang__)
 #define __HAVE_BUILTIN_BSWAP32__
+#define __HAVE_BUILTIN_BSWAP64__
 #endif
-#if GCC_VERSION >= 40800
+#if GCC_VERSION >= 40800 || defined(__clang__)
 #define __HAVE_BUILTIN_BSWAP16__
-#endif
 #endif /* USE_BUILTIN_BSWAP */
 
 
@@ -102,6 +110,15 @@ static inline __attribute__((const)) uint32_t __fswab32(uint32_t val)
 #endif
 }
 
+
+static inline __attribute__((const)) uint64_t __fswab64(uint64_t val)
+{
+#ifdef __HAVE_BUILTIN_BSWAP64__
+	return __builtin_bswap64(val);
+#else
+	return ___constant_swab64(val);
+#endif
+}
 
 /**
  * @brief return a byteswapped 16-bit value
@@ -126,6 +143,16 @@ static inline __attribute__((const)) uint32_t __fswab32(uint32_t val)
 
 
 /**
+ * @brief return a byteswapped 64-bit value
+ * @param x a value to byteswap
+ */
+
+#define __swab64(x)				\
+	(__builtin_constant_p((uint64_t)(x)) ?	\
+	___constant_swab64(x) :			\
+	__fswab64(x))
+
+/**
  * @brief return a byteswapped 16-bit value from a pointer
  * @param p a pointer to a naturally-aligned 16-bit value
  */
@@ -142,6 +169,16 @@ static inline uint16_t __swab16p(const uint16_t *p)
 static inline uint32_t __swab32p(const uint32_t *p)
 {
 	return __swab32(*p);
+}
+
+
+/**
+ * @brief return a byteswapped 64-bit value from a pointer
+ * @param p a pointer to a naturally-aligned 64-bit value
+ */
+static inline uint64_t __swab64p(const uint64_t *p)
+{
+	return __swab64(*p);
 }
 
 
@@ -167,34 +204,52 @@ static inline void __swab32s(uint32_t *p)
 }
 
 
+/**
+ * @brief byteswap a 64-bit value in-place
+ * @param p a pointer to a naturally-aligned 64-bit value
+ */
+
+static inline void __swab64s(uint64_t *p)
+{
+	*p = __swab64p(p);
+}
+
 
 #ifdef __BIG_ENDIAN
 
 #define __cpu_to_le16(x)   ((uint16_t)__swab16((x)))
 #define __cpu_to_le32(x)   ((uint32_t)__swab32((x)))
+#define __cpu_to_le64(x)   ((uint64_t)__swab64((x)))
 
 #define __cpu_to_le16s(x)  __swab16s((x))
 #define __cpu_to_le32s(x)  __swab32s((x))
+#define __cpu_to_le64s(x)  __swab64s((x))
 
 #define __cpu_to_be16(x)   ((uint16_t)(x))
 #define __cpu_to_be32(x)   ((uint32_t)(x))
+#define __cpu_to_be64(x)   ((uint64_t)(x))
 
 #define __cpu_to_be16s(x)  { (void)(x); }
 #define __cpu_to_be32s(x)  { (void)(x); }
+#define __cpu_to_be64s(x)  { (void)(x); }
 
 
 
 #define __le16_to_cpu(x)   __swab16((uint16_t)(x))
 #define __le32_to_cpu(x)   __swab32((uint32_t)(x))
+#define __le64_to_cpu(x)   __swab64((uint64_t)(x))
 
 #define __le16_to_cpus(x)  __swab16s((x))
 #define __le32_to_cpus(x)  __swab32s((x))
+#define __le64_to_cpus(x)  __swab64s((x))
 
 #define __be16_to_cpu(x)   ((uint16_t)(x))
 #define __be32_to_cpu(x)   ((uint32_t)(x))
+#define __be64_to_cpu(x)   ((uint64_t)(x))
 
 #define __be16_to_cpus(x)  { (void)(x); }
 #define __be32_to_cpus(x)  { (void)(x); }
+#define __be64_to_cpus(x)  { (void)(x); }
 
 #endif /* __BIG_ENDIAN */
 
@@ -203,29 +258,37 @@ static inline void __swab32s(uint32_t *p)
 
 #define __cpu_to_le16(x)   ((uint16_t)(x))
 #define __cpu_to_le32(x)   ((uint32_t)(x))
+#define __cpu_to_le64(x)   ((uint64_t)(x))
 
 #define __cpu_to_le16s(x)  { (void)(x); }
 #define __cpu_to_le32s(x)  { (void)(x); }
+#define __cpu_to_le64s(x)  { (void)(x); }
 
 #define __cpu_to_be16(x)   ((uint16_t)__swab16((x)))
 #define __cpu_to_be32(x)   ((uint32_t)__swab32((x)))
+#define __cpu_to_be64(x)   ((uint64_t)__swab64((x)))
 
 #define __cpu_to_be16s(x)  __swab16s((x))
 #define __cpu_to_be32s(x)  __swab32s((x))
+#define __cpu_to_be64s(x)  __swab64s((x))
 
 
 
 #define __le16_to_cpu(x)  ((uint16_t)(x))
 #define __le32_to_cpu(x)  ((uint32_t)(x))
+#define __le64_to_cpu(x)  ((uint64_t)(x))
 
+#define __le64_to_cpus(x) { (void)(x); }
 #define __le32_to_cpus(x) { (void)(x); }
 #define __le16_to_cpus(x) { (void)(x); }
 
 #define __be16_to_cpu(x)  __swab16((uint16_t)(uint16_t)(x))
 #define __be32_to_cpu(x)  __swab32((uint32_t)(uint32_t)(x))
+#define __be64_to_cpu(x)  __swab64((uint64_t)(uint64_t)(x))
 
 #define __be16_to_cpus(x) __swab16s((x))
 #define __be32_to_cpus(x) __swab32s((x))
+#define __be64_to_cpus(x) __swab64s((x))
 
 #endif /* __LITTLE_ENDIAN */
 
@@ -236,18 +299,22 @@ static inline void __swab32s(uint32_t *p)
 /** convert cpu order to little endian */
 #define cpu_to_le16  __cpu_to_le16
 #define cpu_to_le32  __cpu_to_le32
+#define cpu_to_le64  __cpu_to_le64
 
 /** in-place convert cpu order to little endian */
 #define cpu_to_le16s __cpu_to_le16s
 #define cpu_to_le32s __cpu_to_le32s
+#define cpu_to_le64s __cpu_to_le64s
 
 /** convert cpu order to big endian */
 #define cpu_to_be16  __cpu_to_be16
 #define cpu_to_be32  __cpu_to_be32
+#define cpu_to_be64  __cpu_to_be64
 
 /** in-place convert cpu order to big endian */
 #define cpu_to_be16s __cpu_to_be16s
 #define cpu_to_be32s __cpu_to_be32s
+#define cpu_to_be64s __cpu_to_be64s
 
 
 /* same, but in reverse */
@@ -255,18 +322,22 @@ static inline void __swab32s(uint32_t *p)
 /** convert little endian to cpu order*/
 #define le16_to_cpu  __le16_to_cpu
 #define le32_to_cpu  __le32_to_cpu
+#define le64_to_cpu  __le64_to_cpu
 
 /** in-place convert little endian to cpu order*/
 #define le16_to_cpus __le16_to_cpus
 #define le32_to_cpus __le32_to_cpus
+#define le64_to_cpus __le64_to_cpus
 
 /** convert big endian to cpu order*/
 #define be16_to_cpu  __be16_to_cpu
 #define be32_to_cpu  __be32_to_cpu
+#define be64_to_cpu  __be64_to_cpu
 
 /** in-place convert big endian to cpu order*/
 #define be16_to_cpus __be16_to_cpus
 #define be32_to_cpus __be32_to_cpus
+#define be64_to_cpus __be64_to_cpus
 
 
 
