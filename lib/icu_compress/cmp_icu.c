2a8b1f10dfdee5b27b119be553faa3e05d572224
Dominik Loidolt <dominik.loidolt@univie.ac.at>
2024-08-23 11:36:50 +0200

    Fix small typos

--- lib/icu_compress/cmp_icu.c
+++ lib/icu_compress/cmp_icu.c
@@ -2053,7 +2053,7 @@ void compress_chunk_init(uint64_t(return_timestamp)(void), uint32_t version_id)
  * @param dst			destination pointer to the compressed data
  *				buffer; has to be 4-byte aligned; can be NULL to
  *				only get the compressed data size
- * @param dst_capacity		capacity of the dst buffer;  it's recommended to
+ * @param dst_capacity		capacity of the dst buffer; it's recommended to
  *				provide a dst_capacity >=
  *				compress_chunk_cmp_size_bound(chunk, chunk_size)
  *				as it eliminates one potential failure scenario:
